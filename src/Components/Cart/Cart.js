e9864eca37d90afeb7ca81df3e3291c3021b6966
zabihullah <zabihullahyaqubi58@gmail.com>
2024-07-03 22:25:09 +0430

    where we are

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,16 +1,29 @@
 import { useContext } from "react";
 import React from "react";
+import CartIte from "./CartItem"
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../Store/cart-context";
+
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const CartItem = (
     <ul className={classes.listitem}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartIte
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
